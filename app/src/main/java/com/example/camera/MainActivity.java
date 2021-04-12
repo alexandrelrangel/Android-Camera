d190107aa6bc1b07eab82751794fafb41a3eec66
alexandrelrangel <alexandrelrangel@gmail.com>
2021-04-12 15:17:53 -0300

    projeto da CÂMERA completo, já gravando a foto

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.camera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -46,6 +55,34 @@ public class MainActivity extends AppCompatActivity {
             Bundle parametros = data.getExtras();
             Bitmap bitmapFoto = (Bitmap) parametros.get("data");
             novaFoto.setImageBitmap(bitmapFoto);
+
+            // Checando se o APP tem permissão de escrita
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+
+            try {
+                // Cria um STREAM e comprime a foto nele
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmapFoto.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+                // Converte em um Array
+                byte[] bytes = stream.toByteArray();
+
+                // Nome do arquivo com PATH completo
+                String nomeArquivo = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() +
+                        File.separator + "Camera" + File.separator + "IMG_"+String.valueOf(System.currentTimeMillis()) + ".jpg";
+
+                Toast.makeText(this, nomeArquivo, Toast.LENGTH_LONG).show();
+
+                // Grava o arquivo físico
+                FileOutputStream fos = new FileOutputStream(nomeArquivo);
+                fos.write(bytes);
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
